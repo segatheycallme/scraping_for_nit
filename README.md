71a42bb44ce79e4200cd74269cf165287e719b12
segatcm <segatheycallme@proton.me>
2024-07-12 13:41:34 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# scraping for nit
+
+Tool to web scrape products from [SportVision](https://sportvision.rs) and save them to a JSON file.
+
+## Usage
+
+```sh
+/path/to/executable NUMBER_OF_PAGES
+```
---
+++ src/main.rs
@@ -0,0 +1,110 @@
+use std::{env, fs, io::Write};
+
+use reqwest::{Client, RequestBuilder};
+use scraper::{selectable::Selectable, ElementRef, Html, Selector};
+use serde::Serialize;
+use tokio::task::{self, JoinHandle};
+
+#[derive(Serialize, Debug, Clone)]
+struct SportVisionProduct {
+    image_url: String,
+    brand_name: String,
+    title: String,
+    short_description: String,
+    current_price: String,
+}
+
+impl SportVisionProduct {
+    fn from_div(div: ElementRef) -> SportVisionProduct {
+        let mut image_url = String::from(
+            div.select(&Selector::parse(".img-wrapper img").unwrap())
+                .next()
+                .unwrap()
+                .attr("src")
+                .unwrap()
+                .trim(),
+        );
+        image_url.insert_str(0, "https://sportvision.rs");
+        let brand_name = (if let Some(innerr) = div
+            .select(&Selector::parse(".text-wrapper .brand a").unwrap())
+            .next()
+        {
+            innerr.inner_html()
+        } else {
+            "".to_string()
+        })
+        .trim()
+        .to_string();
+        let title = String::from(
+            div.select(&Selector::parse(".text-wrapper .title a").unwrap())
+                .next()
+                .unwrap()
+                .text()
+                .nth(1)
+                .unwrap()
+                .trim(),
+        );
+        let short_description = String::from(
+            div.select(&Selector::parse(".product-shortname").unwrap())
+                .next()
+                .unwrap()
+                .text()
+                .next()
+                .unwrap_or_default()
+                .trim(),
+        );
+        let current_price = String::from(
+            div.select(&Selector::parse(".prices-wrapper .current-price").unwrap())
+                .next()
+                .unwrap()
+                .text()
+                .nth(1)
+                .unwrap()
+                .trim(),
+        );
+        SportVisionProduct {
+            image_url,
+            brand_name,
+            title,
+            short_description,
+            current_price,
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let last_page: u16 = env::args()
+        .nth(1)
+        .expect("Expected 1 argument, got 0")
+        .parse()
+        .expect("Argument should be a non-negative number");
+
+    let mut products: Vec<SportVisionProduct> = vec![];
+    let klijent = Client::new();
+    let mut join_handles: Vec<JoinHandle<Vec<SportVisionProduct>>> = vec![];
+
+    for page in 0..=last_page {
+        let request = klijent.get(format!("https://sportvision.rs/proizvodi/page-{page}"));
+        join_handles.push(task::spawn(get_products(request)));
+    }
+    for handle in join_handles {
+        products.extend(handle.await.unwrap());
+    }
+
+    let mut file = fs::File::create("products.json").expect("Couldn't open/create products.json");
+    file.write_all(serde_json::to_string_pretty(&products).unwrap().as_bytes())
+        .expect("Couldn't write to products.json");
+}
+
+async fn get_products(request: RequestBuilder) -> Vec<SportVisionProduct> {
+    let mut products: Vec<SportVisionProduct> = vec![];
+    let document = Html::parse_document(&request.send().await.unwrap().text().await.unwrap());
+    for div in document
+        .select(&Selector::parse(".wrapper-gridthree-view.product-item .row .item-data").unwrap())
+    {
+        products.push(SportVisionProduct::from_div(div));
+    }
+
+    products
+}
